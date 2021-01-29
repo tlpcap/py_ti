8fd7f481bcc39f2466301776d1b8a60c7c003a6d
MC <mc@tlpcap.com>
2021-01-29 14:17:30 -0500

    Update py_ti.py

--- py_ti/py_ti.py
+++ py_ti/py_ti.py
@@ -298,3 +298,71 @@ def atr_percent(df, column='close', n=20, ma_method='sma',
         return atr_prcnt.to_frame(name=f'atr_%({n})')
     else:
         return atr_prcnt.to_numpy()
+
+    
+def keltner_channel(df, column='close', n=20, ma_method='sma',
+                    upper_factor=2.0, lower_factor=2.0,
+                    add_col=False, return_struct='numpy'):
+    """ Keltner Channels
+    
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        A Dataframe containing the columns open/high/low/close/volume
+        with the index being a date.  open/high/low/close should all
+        be floats.  volume should be an int.  The date index should be
+        a Datetime.
+    column : String, optional. The default is 'close'.
+        This is the name of the column you want to use as the denominator
+        of the percentage calculatioin.
+    n : Int, optional. The default is 20.
+        The lookback period.
+    ma_method : String, optional.  The default is 'sma'
+        The method of smoothing the True Range.  Available smoothing
+        methods: {'sma', 'ema', 'wma', 'hma', 'wilders'}
+    upper_factor : Float, optional.  The default is 2.0
+        The amount by which to multiply the ATR to create the upper channel.
+    lower_factor : Float, optional.  The default is 2.0
+        The amount by which to multiply the ATR to create the lower channel.
+    add_col : Boolean, optional. The default is False.
+        By default the function will return a numpy array. If set to True,
+        the function will add a column to the dataframe that was passed
+        in to it instead or returning a numpy array.
+    return_struct : String, optional. The default is 'numpy'.
+        Only two values accepted: 'numpy' and 'pandas'.  If set to
+        'pandas', a new dataframe will be returned.
+
+    Returns
+    -------
+    There are 3 ways to return values from this function:
+    1. add_col=False, return_struct='numpy' returns a numpy array (default)
+    2. add_col=False, return_struct='pandas' returns a new dataframe
+    3. add_col=True, adds a column to the dataframe that was passed in.
+    
+    Note: If add_col=True the function exits and does not execute the
+    return_struct parameter.
+    """
+    
+    check_errors(df=df, column=column, n=n, ma_method=ma_method,
+                 upper_factor=upper_factor, lower_factor=lower_factor,
+                 add_col=add_col, return_struct=return_struct)
+
+    _ma_func = utils.moving_average_mapper(ma_method)
+    
+    _ma = _ma_func(df, column=column, n=n)
+    _atr = atr(df, n=n, ma_method=ma_method)
+
+    keltner_upper = _ma + (_atr * upper_factor)
+    keltner_lower = _ma - (_atr * lower_factor)
+    keltner = np.vstack((keltner_lower, keltner_upper)).transpose()
+
+    if add_col == True:
+        df[f'kelt({n})_lower'] = keltner_lower
+        df[f'kelt({n})_upper'] = keltner_upper
+        return df
+    elif return_struct == 'pandas':
+        return pd.DataFrame(keltner,
+                            columns=[f'kelt({n})_lower', f'kelt({n})_upper'],
+                            index=df.index)
+    else:
+        return keltner
