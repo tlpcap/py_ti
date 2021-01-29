1d55482854beaaf56b7120d24cf5e4258154cf5a
MC <mc@tlpcap.com>
2021-01-29 17:49:41 -0500

    Update keltner_channels.py

--- examples/keltner_channels.py
+++ examples/keltner_channels.py
@@ -14,8 +14,8 @@ keltner_numpy_array = ti.keltner_channel(data) # same as ti.keltner_channel(data
 # 1 column for each channel
 keltner_dataframe = ti.keltner_channel(data, return_struct='pandas')  # same as ti.keltner_channel(data, n=20, add_col=False, return_struct='pandas')
 
-# add a 20 period Keltner Channels with SMA smoothing to the Dataframe that was passed in
+# add 20 period Keltner Channels with SMA smoothing (default) to the Dataframe that was passed in
 ti.keltner_channel(data, add_col=True) # same as ti.keltner_channel(data, n=20, add_col=True)
 
-# add a 14 period Keltner Channels with Wilders MA smoothing to the Dataframe that was passed in
+# add 14 period Keltner Channels with Wilders MA smoothing to the Dataframe that was passed in
 ti.keltner_channels(data, n=14, ma_method='wilders', add_col=True)
