6f3a9c53957c28fe4ec817f0d31c04aeb03c6edf
MC <mc@tlpcap.com>
2021-03-22 14:12:11 -0400

    Update utils.py

--- py_ti/utils.py
+++ py_ti/utils.py
@@ -4,12 +4,15 @@ def moving_average_mapper(moving_average):
     """
     Map input strings to functions
     """
-    moving_average_funcs = {'sma': ma.sma,
-                            'ema': ma.ema,
-                            'wma': ma.wma,
-                            'hma': ma.hma,
-                            'wilders': ma.wilders_ma,
-                            'kama': ma.kama}
+    moving_average_funcs = {
+        'sma': ma.sma,
+        'ema': ma.ema,
+        'wma': ma.wma,
+        'hma': ma.hma,
+        'wilders': ma.wilders_ma,
+        'kama': ma.kama,
+        'fma': ma.fma,
+        }
 
     moving_average = moving_average_funcs[moving_average]
 
